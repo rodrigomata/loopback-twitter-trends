206920a5a96de039f35d9273d7824fa11637f564
Rodrigo Mata <rodrigo.mata.ib@gmail.com>
2018-07-30 10:08:07 -0500

    feat: switched to async/await

--- server/boot/observer-oauth.js
+++ server/boot/observer-oauth.js
@@ -4,12 +4,15 @@ const request = require('request');
 module.exports = app => {
   const TwitterConnector = app.dataSources.twitter.connector;
   return TwitterConnector.observe('before execute', (ctx, next) => {
-    console.info('Setting OAuth header...');
-    if(!process.env.TWITTER_ACCESS_TOKEN) {
-      console.info('No token found, requesting a new one...');
-      const raw = `${process.env.TWITTER_CONSUMER_KEY}:${process.env.TWITTER_CONSUMER_SECRET}`;
+    const {
+      TWITTER_ACCESS_TOKEN,
+      TWITTER_CONSUMER_KEY,
+      TWITTER_CONSUMER_SECRET
+    } = process.env;
+    if (!TWITTER_ACCESS_TOKEN) {
+      const raw = `${TWITTER_CONSUMER_KEY}:${TWITTER_CONSUMER_SECRET}`;
       // Exception if credentials haven't been set
-      if(!raw) {
+      if (!raw) {
         console.error('Export your Twitter consumer key and secrets before!');
         const error = new Error();
         error.status = 403;
@@ -24,18 +27,23 @@ module.exports = app => {
           'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
           'Authorization': `Basic ${encripted}`
         },
-        form: { grant_type: 'client_credentials' }
+        form: {
+          grant_type: 'client_credentials'
+        }
       }, (err, response, body) => {
-        if(err) throw err;
-        console.info('Token requested manually...');
-        let parser = JSON.parse(body);
+        if (err) throw err;
+        const parser = JSON.parse(body);
         process.env['TWITTER_ACCESS_TOKEN'] = parser.access_token;
-        ctx.req.headers = { 'Authorization': `Bearer ${process.env.TWITTER_ACCESS_TOKEN}` };
+        ctx.req.headers = {
+          'Authorization': `Bearer ${TWITTER_ACCESS_TOKEN}`
+        };
         return next();
       });
     } else {
       console.info('Token detected and set automatically...');
-      ctx.req.headers = { 'Authorization': `Bearer ${process.env.TWITTER_ACCESS_TOKEN}` };
+      ctx.req.headers = {
+        'Authorization': `Bearer ${TWITTER_ACCESS_TOKEN}`
+      };
       return next();
     }
   });
--- server/models/trend.js
+++ server/models/trend.js
@@ -1,26 +1,36 @@
 'use strict';
-const async = require('async');
 
 module.exports = Trend => {
   // Remote Method
-  Trend.getTrends = (id = 1, cb) => {
-    async.waterfall([
-      cb => Trend.app.datasources.twitter.getTrends(id, (err, results) => (err) ? cb(err) : cb(null, results[0].trends)),
-      (trend_list, cb) => cb(null, trend_list.sort((a,b) => a.tweet_volume < b.tweet_volume))
-    ], cb);
+  Trend.getTrends = async function (countryId = 1, cb) {
+    try {
+      const results = await Trend.app.datasources.twitter.getTrends(countryId);
+      return results[0].trends.sort((a, b) => a.tweet_volume < b.tweet_volume);
+    } catch (err) {
+      cb(err);
+    }
   };
 
   // Expose Remote Method
   Trend.remoteMethod('getTrends', {
-    http: { path: '/:countryId', verb: 'get' },
+    http: {
+      path: '/:countryId',
+      verb: 'get'
+    },
     accepts: [{
       arg: 'countryId',
       type: 'integer',
       description: 'WOEID',
       required: true,
-      http: { source: 'path' }
+      http: {
+        source: 'path'
+      }
     }],
-    returns: { arg: 'trends', type: 'Trend', root: true },
+    returns: {
+      arg: 'trends',
+      type: 'Trend',
+      root: true
+    },
     description: 'Finds the Twitter Trends of a given place'
   });
 };
